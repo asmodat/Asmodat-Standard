64f162d9736559d42c092fe69b310c05c4e31e53
asmodat <asmodat@hotmail.com>
2017-12-27 23:55:13 +0100

    2017-12-27

---
+++ Asmodat Standard/Extensions/BoolEx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace AsmodatStandard.Extensions
+{
+    public static class BoolEx
+    {
+        /// <summary>
+        /// true == 1 | false == 0
+        /// </summary>
+        public static int ToInt(this bool b) => b ? 1 : 0;
+
+        /// <summary>
+        /// true == 1 | false == 0 or null
+        /// </summary>
+        public static int ToInt(this bool? b) => b != null && b.Value ? 1 : 0;
+
+        /// <summary>
+        /// true == 1 else false
+        /// </summary>
+        public static int ToBool(this Int32 v) => v == 1 ? 1 : 0;
+    }
+}
