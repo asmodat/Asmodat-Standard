0f43cda9f192bcb389c4774d27f5057ebb59c8b7
asmodat <asmodat@hotmail.com>
2018-07-25 13:45:34 +0200

    2018-07-25

---
+++ Asmodat Standard/Extensions/NUnit/Attributes/LocalTestAttribute.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace AsmodatStandard.Extensions.NUnit.Attributes
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+    public class LocalTestAttribute : CategoryAttribute
+    { } 
+}
