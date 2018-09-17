4538242ea462d1eb1395041ee7a02027048ea4d0
asmodat <asmodat@hotmail.com>
2018-09-17 12:53:15 +0200

    2018-09-17

--- Asmodat Standard/Extensions/Net/DnsEx.cs
+++ Asmodat Standard/Extensions/Net/DnsEx.cs
@@ -8,11 +8,40 @@ using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace AsmodatStandard.Extensions.Net
 {
     public static class DnsEx
     {
+        public static async Task<string[]> GetHostAddressesAsync(string host, int timeout)
+        {
+            Exception lastException = null;
+            var sw = Stopwatch.StartNew();
+            string[] hostAddressess = null;
+            do
+            {
+                try
+                {
+                    hostAddressess = (await Dns.GetHostAddressesAsync(host))?
+                        .Where(x => (x?.ToString()).IsNullOrEmpty() == false)
+                        .Select(x => x.ToString()).ToArray();
+
+                    if (!hostAddressess.IsNullOrEmpty())
+                        return hostAddressess;
+                }
+                catch (Exception ex)
+                {
+                    lastException = ex;
+                }
+
+                await Task.Delay(1000);
+
+            } while (sw.ElapsedMilliseconds < timeout);
+
+            throw new Exception($"Timeout, Could not find Addressess for DNS Host Name: '{host}', Elapsed: {sw.ElapsedMilliseconds}/{timeout} [ms].", lastException);
+        }
+
         public static string GetHostName(string uri, int timeout)
         {
             Exception lastException = null;
