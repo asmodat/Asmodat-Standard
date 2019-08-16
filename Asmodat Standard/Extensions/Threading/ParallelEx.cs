df54480ea7fa679f6cba712c4272538316af5da2
asmodat <asmodat@gmail.com>
2019-08-16 23:17:20 +0200

    2019-08-16

--- Asmodat Standard/Extensions/Threading/ParallelEx.cs
+++ Asmodat Standard/Extensions/Threading/ParallelEx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AsmodatStandard.Extensions.Collections;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
@@ -14,8 +15,22 @@ namespace AsmodatStandard.Extensions.Threading
         public static async Task ForAsync(int from, int to, Func<int, Task> func)
             => await Task.WhenAll(Enumerable.Range(from, to - from).Select(i => func(i)));
 
-        public static async Task ForEachAsync<K>(IEnumerable<K> source, Func<K, Task> func)
-            => await Task.WhenAll(source.Select(x => func(x)));
+        public static async Task ForEachAsync<K>(IEnumerable<K> source, Func<K, Task> func, int maxDegreeOfParallelism = 0)
+        {
+            var list = new List<K>();
+            foreach (var f in source)
+            {
+                list.Add(f);
+                if (list.Count >= maxDegreeOfParallelism)
+                {
+                    await Task.WhenAll(list.Select(x => func(x)));
+                    list.Clear();
+                }
+            }
+
+            if (!list.IsNullOrEmpty())
+                await Task.WhenAll(list.Select(x => func(x)));
+        }
 
         public static async Task ForEachAsync<K>(IEnumerable<K> source, Action<K> action)
             => await Task.WhenAll(source.Select(x => new Task(() => action(x))));
---
+++ Asmodat Standard/Types/SerializableFileInfo.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using AsmodatStandard.Extensions;
+
+namespace AsmodatStandard.Types
+{
+    public static class SerializableFileInfoEx
+    {
+        public static SerializableFileInfo ToSerializableFileInfo(this FileInfo fi, string md5)
+        {
+            var sfi = new SerializableFileInfo();
+            sfi.Attributes = fi.Attributes;
+            sfi.CreationTime = fi.CreationTimeUtc.ToUnixTimestamp();
+            sfi.LastAccessTime = fi.LastAccessTimeUtc.ToUnixTimestamp();
+            sfi.LastWriteTime = fi.LastWriteTimeUtc.ToUnixTimestamp();
+
+            sfi.DirectoryFullName = fi.Directory.FullName;
+            sfi.DirectoryName = fi.DirectoryName;
+            sfi.Exists = fi.Exists;
+            sfi.Extension = fi.Extension;
+            sfi.FullName = fi.FullName;
+            sfi.IsReadOnly = fi.IsReadOnly;
+  
+            sfi.Length = fi.Length;
+            sfi.Name = fi.Name;
+            sfi.MD5 = md5;
+
+            return sfi;
+        }
+    }
+
+    public class SerializableFileInfo
+    {
+        public FileAttributes Attributes { get; set; }
+
+        public long CreationTime { get; set; }
+        public long LastAccessTime { get; set; }
+        public long LastWriteTime { get; set; }
+        public string Name { get; set; }
+
+        public string DirectoryFullName { get; set; }
+        public string DirectoryName { get; set; }
+        public string Extension { get; set; }
+        public string FullName { get; set; }
+
+        public bool Exists { get; set; }
+        public bool IsReadOnly { get; set; }
+
+        public long Length { get; set; }
+        
+        public string MD5 { get; set; }
+    }
+}
